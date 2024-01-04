7b4c43c07f700e92723b483d1bf70d5e925382bc
Jucagovi <jc.gomez@edu.gva.es>
2024-01-04 18:42:35 +0100

    Listado de una tabla

--- src/contextos/ProveedorFeos.jsx
+++ src/contextos/ProveedorFeos.jsx
@@ -20,75 +20,16 @@ const ContextoFeos = createContext();
 
 const ProveedorFeos = ({ children }) => {
   /** Valores iniciales para los estados */
-  const objetoInicial = {};
-  const arrayInicial = [];
-  const cadenaCargando = "Cargando datos...";
-  const cadenaInicial = "";
 
   /** Estados para proveer. */
-  const [listadoFeos, setListado] = useState(arrayInicial);
-  const [situacion, setSituacion] = useState(cadenaCargando);
-
-  /**
-   * Función para obtener el listado de "feos"
-   * usando la conexión al servicio supabase.
-   */
-  const obtenerListadoSencillo = async () => {
-    try {
-      const { data, error } = await supabaseConexion.from("Feos").select("*");
-      setListado(data);
-    } catch (fallo) {
-      setSituacion(fallo.message);
-    }
-  };
-
-  /**
-   * Función para filtrar el listado de "feos"
-   * usando el operador eq().
-   */
-  const filtrarFeos = async () => {
-    try {
-      const { data, error } = await supabaseConexion
-        .from("Feos")
-        .select("*")
-        .eq("country", "Burkina Faso");
-      setListadoFeos(data);
-    } catch (fallo) {
-      setSituacion(fallo.message);
-    }
-  };
-
-  /**
-   * Función para ordenar el listado de "feos"
-   * usando la cláusula order.
-   */
-  const ordenarFeos = async (orden) => {
-    try {
-      const { data, error } = await supabaseConexion
-        .from("Feos")
-        .select("*")
-        .order("name", { ascending: orden });
-      setListadoFeos(data);
-    } catch (fallo) {
-      setSituacion(fallo.message);
-    }
-  };
 
   /** useEffect con las tareas a realizar en la carga del documento. */
-  useEffect(() => {
-    obtenerListadoSencillo();
-  }, []);
+  useEffect(() => {}, []);
 
   /**
    * Objeto con la información a exportar
    */
-  const datosAExportar = {
-    listadoFeos,
-    situacion,
-    obtenerListadoSencillo,
-    filtrarFeos,
-    ordenarFeos,
-  };
+  const datosAExportar = {};
 
   return (
     <ContextoFeos.Provider value={datosAExportar}>
