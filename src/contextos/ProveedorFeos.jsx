00b1f7f03613def692115db4ca8d08274b0e79e6
Jucagovi <jc.gomez@edu.gva.es>
2024-01-04 18:54:10 +0100

    Inserción de registros

--- src/contextos/ProveedorFeos.jsx
+++ src/contextos/ProveedorFeos.jsx
@@ -1,5 +1,17 @@
 import React, { useState, useEffect, createContext } from "react";
+/** Se importa la conexión al servicio Supabase. */
 import { supabaseConexion } from "../config/supabase.js";
+
+/** Nuevo enfoque a la hora de crear contextos:
+ *
+ *  ->  se crea el contexto con la palabra "contexto"
+ *      y una referencia a los datos que contiene (esto no cambia).
+ *
+ *  ->  al componente que provee la información
+ *      se le cambia el nombre a "Proveedor" seguido de una
+ *      referencia al tipo de datos que contiene. De este modo
+ *      quedan diferenciadas las funcionalidades a través del nombre.
+ */
 
 /**
  * Se crea el contexto en el ámbito global (fuera del componente).
@@ -11,6 +23,7 @@ const ProveedorFeos = ({ children }) => {
   const objetoInicial = {};
   const arrayInicial = [];
   const cadenaCargando = "Cargando datos...";
+  const cadenaInicial = "";
 
   /** Estados para proveer. */
   const [listadoFeos, setListado] = useState(arrayInicial);
@@ -29,6 +42,38 @@ const ProveedorFeos = ({ children }) => {
     }
   };
 
+  /**
+   * Función para filtrar el listado de "feos"
+   * usando el operador eq().
+   */
+  const filtrarFeos = async () => {
+    try {
+      const { data, error } = await supabaseConexion
+        .from("Feos")
+        .select("*")
+        .eq("country", "Burkina Faso");
+      setListadoFeos(data);
+    } catch (fallo) {
+      setSituacion(fallo.message);
+    }
+  };
+
+  /**
+   * Función para ordenar el listado de "feos"
+   * usando la cláusula order.
+   */
+  const ordenarFeos = async (orden) => {
+    try {
+      const { data, error } = await supabaseConexion
+        .from("Feos")
+        .select("*")
+        .order("name", { ascending: orden });
+      setListadoFeos(data);
+    } catch (fallo) {
+      setSituacion(fallo.message);
+    }
+  };
+
   /** useEffect con las tareas a realizar en la carga del documento. */
   useEffect(() => {
     obtenerListadoSencillo();
@@ -37,7 +82,13 @@ const ProveedorFeos = ({ children }) => {
   /**
    * Objeto con la información a exportar
    */
-  const datosAExportar = { listadoFeos, situacion };
+  const datosAExportar = {
+    listadoFeos,
+    situacion,
+    obtenerListadoSencillo,
+    filtrarFeos,
+    ordenarFeos,
+  };
 
   return (
     <ContextoFeos.Provider value={datosAExportar}>
