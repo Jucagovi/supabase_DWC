e894e748a3b0f44981c4943e5affaaa815dcc57d
Jucagovi <jc.gomez@edu.gva.es>
2024-01-04 19:12:41 +0100

    Actualización de registros

--- src/componentes/ListadoFeosSencillo.jsx
+++ src/componentes/ListadoFeosSencillo.jsx
@@ -1,19 +1,58 @@
 import React, { Fragment } from "react";
+import "./ListadoFeosSencillo.css";
 import ListadoFeoSencillo from "./ListadoFeoSencillo.jsx";
 import useFeos from "../hooks/useFeos.js";
 
 const ListadoFeosSencillo = () => {
-  const { listadoFeos, situacion } = useFeos();
+  const {
+    listadoFeos,
+    situacion,
+    obtenerListadoSencillo,
+    filtrarFeos,
+    ordenarFeos,
+  } = useFeos();
 
   return (
     <Fragment>
-      <h3>Listado de Feos.</h3>
-      <div id='listado'>
-        {listadoFeos.length
-          ? listadoFeos.map((valor, indice, array) => {
-              return <ListadoFeoSencillo key={valor.id} datos={valor} />;
-            })
-          : situacion}
+      <div id='listadoSencillo'>
+        <div className='lis-caja'>
+          <h3>Listado de Feos.</h3>
+          {listadoFeos.length
+            ? listadoFeos.map((valor, indice, array) => {
+                return <ListadoFeoSencillo key={valor.id} datos={valor} />;
+              })
+            : situacion}
+        </div>
+        <div className='lis-caja'>
+          <button
+            onClick={() => {
+              obtenerListadoSencillo();
+            }}
+          >
+            Cargar feos
+          </button>
+          <button
+            onClick={() => {
+              ordenarFeos(true);
+            }}
+          >
+            Ordenar feos ascendentemente
+          </button>
+          <button
+            onClick={() => {
+              ordenarFeos(false);
+            }}
+          >
+            Ordenar feos descendentemente
+          </button>
+          <button
+            onClick={() => {
+              filtrarFeos();
+            }}
+          >
+            Filtrar feos de Burkina Faso
+          </button>
+        </div>
       </div>
     </Fragment>
   );
